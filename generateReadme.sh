d19d07bac7b041b42c03860c555d499b512d3ab6
Adrian Jagnanan <ajagnanan@gmail.com>
2024-09-25 18:18:42 -0500

    Adding in docs

--- generateReadme.sh
+++ generateReadme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # https://github.com/norwoodj/helm-docs
-helm-docs --chart-search-root=charts --template-files ./README.md.gotmpl
+helm-docs --chart-search-root=./charts --template-files=./README.md.gotmpl
